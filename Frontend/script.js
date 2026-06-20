2dc60139f4f592e8c813356086590d982aaf5a34
Rajesh Reddy <244g1a05v9@srit.ac.in>
2026-06-20 18:59:19 +0530

    Added Citizen page

--- Frontend/script.js
+++ Frontend/script.js
@@ -8,3 +8,47 @@ alert(
 );
 
 };
+function addCitizen(){
+
+let name =
+document
+.getElementById("name")
+.value;
+
+let city =
+document
+.getElementById("city")
+.value;
+
+if(
+name===""
+||
+city===""
+
+){
+
+alert(
+"Fill fields"
+);
+
+return;
+
+}
+
+let row=
+`
+<tr>
+
+<td>${name}</td>
+
+<td>${city}</td>
+
+</tr>
+`;
+
+document
+.getElementById("table")
+
+.innerHTML += row;
+
+}
